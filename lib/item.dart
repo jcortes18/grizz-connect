3b1e1e482e09e70f488499a5e989b69938d63cb8
ajbrueckner <91899853+ajbrueckner@users.noreply.github.com>
2021-12-05 18:50:46 -0500

    Final Item Page Commit

--- lib/item.dart
+++ lib/item.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'database.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_scroll_to_top/flutter_scroll_to_top.dart';
 
 
 class ItemPage extends StatefulWidget {
@@ -17,7 +19,34 @@ class _ItemPageState extends State<ItemPage> {
   final myController = TextEditingController();
   String comment = '';
 
-  String error='';
+  bool _showBackToTopButton = false;
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        if (_scrollController.offset >= 400) {
+          _showBackToTopButton = true; // show the back-to-top button
+        } else {
+          _showBackToTopButton = false; // hide the back-to-top button
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: Duration(milliseconds: 400), curve: Curves.linear);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +60,13 @@ class _ItemPageState extends State<ItemPage> {
               'Grizz Connect',
             )),
         body: SingleChildScrollView(
+            controller: _scrollController,
+            scrollDirection: Axis.vertical,
           //padding: const EdgeInsets.all(8.0),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   //carousel(context),
-                  //((widget.item['imageURL']!=null)?Image.network(widget.item['imageURL'],fit: BoxFit.fill)
-                    //:
-                  //Icon(Icons.person_rounded, color: Colors.white, size: MediaQuery.of(context).size.width * .4,),
-                  //Image.network("https://upload.wikimedia.org/wikipedia/commons/6/6b/Picture_icon_BLACK.svg"),
                   Image.network(widget.item['imageURL']),
                   Card(color: const Color.fromRGBO(201, 199, 199, 1.0),
                       child: ListTile(title: Text(
@@ -59,12 +86,54 @@ class _ItemPageState extends State<ItemPage> {
                           textAlign: TextAlign.center,
                           style: const TextStyle(
                               fontWeight: FontWeight.bold)),)),
-                  const SizedBox(height: 5.0),
-                  Text(''+error+'',
-                      style: const TextStyle(//height: 0,
-                          color: Colors.red, fontSize: 25.0, //backgroundColor: Colors.transparent,
-                          fontWeight: FontWeight.w700)
-                  ),
+
+                  Row(
+                      children: <Widget> [
+                      Expanded(
+                          child: TextButton(
+                            onPressed: () async{
+                                  var url = "https://venmo.com";
+                                  if (await launch(url)) {
+                                       await launch(url);
+                                   } else {
+                                        throw 'Could not launch $url';
+                                      }}, child: Image.asset('Venmo.jpg', height: 60, width: 60),),
+                                ),
+                      Expanded(
+                          child: TextButton(
+                           onPressed: () async{
+                             var url = "https://www.paypal.com/us/home";
+                             if (await launch(url)) {
+                               await launch(url);
+                             } else {
+                               throw 'Could not launch $url';
+                             }}, child: Image.asset('PayPal.png', height: 60, width: 60),),
+                      ),
+                         Expanded(
+                        child: TextButton(
+                              onPressed: () async{
+                                var url = "https://www.zellepay.com";
+                                if (await launch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }}, child: Image.asset('Zelle.png', height: 60, width: 60),),
+                         ),]),
+
+                  /*FloatingActionButton(child: const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("Venmo.jpg",
+                      ),
+                      ),
+                      onPressed: () async{
+                      var url = "https://venmo.com";
+                      if (await launch(url)) {
+                        await launch(url);
+                      } else {
+                         throw 'Could not launch $url';
+                        }
+                                  }
+                            ),*/
                   TextFormField(validator: (val) {
 
                     if (val!.isNotEmpty) {
@@ -74,7 +143,6 @@ class _ItemPageState extends State<ItemPage> {
                       //validator: (val) => val.length > 5 ? 'Enter an email': 'null';
                       setState(() => comment = val);
                     },
-
                     controller: myController,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
@@ -93,27 +161,10 @@ class _ItemPageState extends State<ItemPage> {
                       final FirebaseAuth _auth = FirebaseAuth.instance;
                       final user = _auth.currentUser;
                       final userid = user!.uid.toString();
-                      if(comment.length < 1){
-                       setState(() {
-                         error = 'short comment';
-                       });
-                      }
 
-                      else {
-                        setState(() {
-                          error = '';
-                        });
-                        await DatabaseService(uid: widget.item.id)
-                            .updateComments(
-                            comment, user.displayName.toString(),
-                            user.email.toString());
-                        myController.clear();
-                      }
-                      setState(() {
-                        comment = '';
-                      });
-                          },
-
+                      await DatabaseService(uid: widget.item.id).updateComments(
+                          comment, user.displayName.toString(), user.email.toString()); myController.clear();
+                    },
                     label: const Text('Add Comment'),
                     icon: const Icon(Icons.add_comment),
                     //.thumb_up),
@@ -143,24 +194,81 @@ class _ItemPageState extends State<ItemPage> {
                         }
                     ),
                   ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15, right: 8.0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                    ),
+                  ),
 
                 ])
-        )
-
+        ),
+      floatingActionButton: _showBackToTopButton == false
+    ? null
+        : FloatingActionButton(
+    onPressed: _scrollToTop,
+      child: const Icon(Icons.arrow_circle_up),
+      backgroundColor: Colors.black,
+      elevation: 10.0,
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+
   }
 }
-/*
-Navigator.push(
-context, MaterialPageRoute(builder: (context) => ItemPage(item: data.docs[index])));
 
-*/
 
-/*
-child: CircularButton(
-icon: const Icon(Icons.house_rounded, size: 32,
-),
-onPressed: () {
-Navigator.pushNamed(context, 'welcome');
-},
-),*/
+
+
+
+// **CODE FOR CUSTOM APP BAR** //
+
+/*_appBar(context, height) => PreferredSize(
+  preferredSize: Size(MediaQuery.of(context).size.width, height+60),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      FloatingActionButton(elevation: 0, child:Image.asset("assets/back_button.png", width: 27, height: 30,),
+          onPressed: (){},
+          backgroundColor: Colors.transparent
+      ),
+      const Center(child: Text("GrizzConnet", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color.fromRGBO(255, 193, 8, 1.0)),),
+      ),
+      FloatingActionButton(elevation: 0, child:Image.asset("assets/bag_button.png", width: 40, height: 40,),
+          onPressed: (){},
+          backgroundColor: Colors.transparent
+      ),
+    ],
+  ),
+);
+// **CODE FOR PICTURES FOR CAROUSEL** //
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+// **CODE FOR CAROUSEL** //
+Widget carousel(context){
+  return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: CarouselSlider(
+        options: CarouselOptions(
+            enlargeCenterPage: true,
+            enableInfiniteScroll: true
+        ),
+        items: imgList.map((i) => ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Image.network(i,
+                    width: 1050,
+                    height: 350,
+                    fit: BoxFit.cover,)
+                ]
+            )
+        )).toList(),
+      )
+  );
+}*/
