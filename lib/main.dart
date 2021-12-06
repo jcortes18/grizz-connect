11f34279b8938fe536394934dc6c2aac689c8bcd
safqar <55568602+safqar@users.noreply.github.com>
2021-12-05 20:22:06 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grizz_connect/login.dart';
 import 'package:grizz_connect/marketplace_main.dart';
+import 'package:grizz_connect/modifylist.dart';
 import 'package:grizz_connect/quiz.dart';
 import 'package:grizz_connect/user_list.dart';
 import 'package:grizz_connect/register.dart';
 import 'package:grizz_connect/start.dart';
 import 'package:grizz_connect/welcome.dart';
 import 'package:grizz_connect/pro.dart';
+import 'package:grizz_connect/modifylist.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
+    debugShowCheckedModeBanner: false,
     home: MyLogin(),
     routes: {
       'register': (context) => MyRegister(),
@@ -25,6 +27,7 @@ void main() async {
       'quiz' : (context) => Quiz(),
       'marketplace_main' : (context) => MarketplaceTab(),
       'welcome' : (context) => MyWelcome(),
+      'modifylist' : (context) => ModifyListTab(),
     },
   ));
 }
